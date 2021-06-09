f35c5a9ae45e80c60c7b75a65ca15addf3e9d086
Sadurr <filipsadurski3@gmail.com>
2021-06-09 15:41:19 +0200

    pattern() function added

--- run.py
+++ run.py
@@ -24,23 +24,19 @@ def contact():
 
 
 @app.route('/user/<username>', methods=['GET', 'POST'])
-
 def show_user_profile(username):
     if request.method == 'POST':
-        return 'HTTP POST for user %s with password %s' %(username, request.form['password'])
+        return 'HTTP POST for user %s with password %s' % (username, request.form['password'])
     else:
-        return 'HTTP GET for user %s' %(username)
+        return 'HTTP GET for user %s' % (username)
 
 
 @app.route("/error_denied")
-
 def error_denied():
     abort(401)
 
 
 @app.route('/error_internal')
-
-
 def error_internal():
     return render_template('template.html', name='ERROR 505'), 505
 
@@ -48,7 +44,7 @@ def error_internal():
 @app.route('/error_not_found')
 def error_not_found():
     response = make_response(render_template('template.html', name='ERROR 404'), 404)
-    response.headers['X-Something']='A value'
+    response.headers['X-Something'] = 'A value'
     return response
 
 
@@ -64,14 +60,21 @@ def login():
     else:
         return render_template("login.html")
 
+
 @app.route('/about')
 def about():
-        return render_template("about.html")
-    
+    return render_template("about.html")
+
 
 @app.route('/gallery')
 def gallery():
     return render_template("gallery.html")
+
+
+@app.route('/s/')
+def pattern():
+    return render_template('index.html', user="Andrzej", email="email@mail.com")
+
 
 if __name__ == "__main__":
     app.run(debug=True)
